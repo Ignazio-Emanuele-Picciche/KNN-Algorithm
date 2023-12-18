68ed51cef9b152be75b3030fcd97ab716ea21da5
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2023-12-18 10:44:16 +0100

    Definizione metodi nella classe KNNAlgorithm : allenamento_dati , predizione_modello, calcolo_distanza_euclidea e costruttore.

--- KNNAlgorithm.py
+++ KNNAlgorithm.py
@@ -1,1 +1,19 @@
 class KNNAlgorithm:
+    k=int # numero di vicinati
+    dist_Euclidea=int  #distanza euclidea
+
+    def __init__(self,k):
+        pass
+
+    def allenamento_dati(self,x_train,y_train): # x_train coordinate dei punti y_train identifica il cluster di appartenenza
+        pass
+
+    def predizione_modello(self,x_test): #x_test coordinate dei test
+        pass   # mi richiamo distanza eulidea
+
+    def calcolo_distanza_euclidea(self):# va fatto per ogni punto di x_test
+        pass
+
+
+
+
