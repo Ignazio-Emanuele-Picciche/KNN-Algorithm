a5c6c53bcd6ae40d77114f01c8a3224294c131e2
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2023-12-27 22:19:41 +0100

    Aggiunti commenti per migliorare la comprensione della classe

--- KNNAlgorithm.py
+++ KNNAlgorithm.py
@@ -1,17 +1,31 @@
 class KNNAlgorithm:
-    k=int # numero di vicinati
-    dist_Euclidea=int  #distanza euclidea
 
     def __init__(self,k):
         pass
 
-    def allenamento_dati(self,x_train,y_train): # x_train coordinate dei punti y_train identifica il cluster di appartenenza
+    def allenamento_dati(self,x_train,y_train): # aquisizione dei dati postprocessati
+        # x_train rappresenta la matice delle features (B a J)
+        # y_train rappresenta le y (K)
         pass
 
-    def predizione_modello(self,x_test): #x_test coordinate dei test
-        pass   # mi richiamo distanza eulidea
+    def predizione_modello(self,x_test):
+        #x_test rappresenta la matrice delle features che sono test
+        # Classificazione in due gruppi (benigno, maligno)
+        ''' confrontare ogni x_test con ogni x_train --> due cicli for
+        in questo mi clacolo la distanza euclidea quindi richiamo il metodo della
+         distanza euclidea
+         per ciascun x_test mi conservo anche la y_test associata , oltre che la distanza
+        '''
+        # ordino in modo crescente le distanze (con associate y_test)
+        # prendo le prime k distanze
+        # analizzo (conto) le y_test e prendo la y_test più numerosa
+        # OSS: nel caso sono di numero uguali ne prendo una a le due a caso
 
-    def calcolo_distanza_euclidea(self):# va fatto per ogni punto di x_test
+        # return: sarebbe il y_test trovata
+
+    def calcolo_distanza_euclidea(self):
+        #return: distanza euclidea tra vettore x_test e x_train
+
         pass
 
 
