7b18b6530e11d5ba85dcfa6c430b26ce75eb9df8
RiccardoPolacchi <riccardopolacchi@gmail.com>
2023-12-28 21:57:29 +0100

    modificato file input, aggiunti metodi 'percentuali di splitting','scelta vicini','scelta metodo evaluation'(da completare)

--- classeInput.py
+++ classeInput.py
@@ -1,7 +1,28 @@
+# __import__(class evaluation)
+
 class input():
-    k = int(input("Inserire il numero di vicini k da utilizzare per il classificatore: ")) #numero di vicini da utilizzare per il classificatore
-    K = int(input("Inserire il numero di esperimenti K: ")) #numero di esperimenti
-    training = int(input("Inserire la percentuale di training: ")) #percentuale di training (la percentuale di test sarà ricavata automaticamente dal programma)
-    test = 100 - training #percentuale di test (ricavata dal programma per sottrazione
-    print(f"La percentuale di test è {test}")
+   # k = int(input("Inserire il numero di vicini k da utilizzare per il classificatore: ")) #numero di vicini da utilizzare per il classificatore
+    #K = int(input("Inserire il numero di esperimenti K: ")) #numero di esperimenti
+    #training = int(input("Inserire la percentuale di training: ")) #percentuale di training (la percentuale di test sarà ricavata automaticamente dal programma)
+    #test = 100 - training #percentuale di test (ricavata dal programma per sottrazione
+    #print(f"La percentuale di test è {test}")
+
+    def percentuali_di_splitting(self):
+        this.training = int(input("Inserire la percentuale di training: ")) #percentuale di training (la percentuale di test sarà ricavata automaticamente dal programma)
+        this.test = 100 - training #percentuale di test (ricavata dal programma per sottrazione
+        print(f"La percentuale di test è {test}")
+        return training,test
+
+    def scelta_vicini_k(self):
+        this.k = int(input("Inserire il numero di vicini k da utilizzare per il classificatore: ")) #numero di vicini da utilizzare per il classificatore
+        return k
+
+    def scelta_metodo_evaluation(self):#l'utente sceglie se utilizzare l'holdout o il random subsampling
+        scelta_effettuata=0
+        while scelta_effettuata !=1 or scelta_effettuata !=2:
+            this.scelta_evaluation = int(input("inserire 1 per scegliere l'holdout, 2 per il random subsampling"))
+
+
+
+
 
