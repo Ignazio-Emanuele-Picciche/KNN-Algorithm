d0c49702debc625dc8619927640e648cbd896817
Ignazio-Emanuele-Picciche <82161529+Ignazio-Emanuele-Picciche@users.noreply.github.com>
2023-12-28 20:17:45 +0100

    scritto i passaggi generali (sotto forma di commenti) per il meotod valutazione_holdout

--- Evaluation.py
+++ Evaluation.py
@@ -20,15 +20,32 @@ class Evaluation:
     def __init__(self):
         pass
 
+    '''   
+    Il processo di valutazione holdout consiste in:
+    1. Dividire i dati: i dati vengono divisi casualmente (viene specificata la percentuale in input dall'utente) in dati di training e dati di test
+    2. Addestramento del modello: il modello quindi si addestra dandogli "in pasto" i dati x_train e y_train
+    3. Valutazione delle performance: il modello appena addestrato viene quindi valutato utilizzando i dati di test (x_test). Le prestazione del modello vengono calcolate tramite diverse metriche
+    4. Analisi dei risultati: si analizzano le metriche trovate per capire quanto il mio modello generalizza sui dati sconosciuti
+    '''
     def valutazione_holdout(self, x_train, y_train, x_test, y_test):
         
-        '''this.x_train = x_train
+        '''
+        this.x_train = x_train
         this.y_train = y_train
         this.x_test = x_test
-        this.y_test = y_test'''
+        this.y_test = y_test
+        '''
 
         pass
 
+    
+    ''' 
+    Il processo di valutazione random subsampling consiste in:
+    1. Specificare in input il numero di esperimenti (K) da effettuare
+    2. Specificare in input la percentuale per i dati di train e test
+    3. 
+    '''
+    
     def valutazione_random_subsampling(self, K):
         # this.K = K # Indica il numero di esperimenti da fare nel caso di valutazione di tipo "Random Subsampling"
         pass
