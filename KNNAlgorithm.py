be25eb1336404d53554cddaf59fda41cc294cc12
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2023-12-30 11:39:14 +0100

    Implementazione dei metodi : allenamento_dati, clacolo_distanza_euclidea

--- KNNAlgorithm.py
+++ KNNAlgorithm.py
@@ -1,12 +1,17 @@
-class KNNAlgorithm:
+import numpy as np
 
+class KNNAlgorithm:
+    import numpy as n
     def __init__(self,k):
-        pass
+        self.k = k
+
 
     def allenamento_dati(self,x_train,y_train): # aquisizione dei dati postprocessati
         # x_train rappresenta la matice delle features (B a J)
-        # y_train rappresenta le y (K)
-        pass
+        # y_train represent le y (K)
+        self.x_train = x_train
+        self.y_train = y_train
+
 
     def predizione_modello(self,x_test):
         #x_test rappresenta la matrice delle features che sono test
@@ -16,6 +21,10 @@ class KNNAlgorithm:
          distanza euclidea
          per ciascun x_test mi conservo anche la y_test associata , oltre che la distanza
         '''
+
+        # for i in x_test:
+        #     for j in self.x_train:
+        #self.calcolo_distanza_euclidea(i,j)
         # ordino in modo crescente le distanze (con associate y_test)
         # prendo le prime k distanze
         # analizzo (conto) le y_test e prendo la y_test più numerosa
@@ -23,10 +32,13 @@ class KNNAlgorithm:
 
         # return: sarebbe il y_test trovata
 
-    def calcolo_distanza_euclidea(self):
+    def calcolo_distanza_euclidea(self,x1,x2):
         #return: distanza euclidea tra vettore x_test e x_train
+        x3=x1-x2
+        distanza= np.sqrt(np.sum(pow(x3,2)))
+        return distanza
 
-        pass
+
 
 
 
