ec5d82f60a436680581a7ba845a37ca7ecf61add
RiccardoPolacchi <riccardopolacchi@gmail.com>
2024-01-02 15:55:21 +0100

    aggiunti commenti sulle funzioni della classe preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#Le funzioni della classe preprocessing sono:
+#caricare il dataset
+#gestire i valori mancanti
+#dividere i dati in features (x) e target label (y)
+#normalizzare o standardizzare i dati a seconda della scelta
+#passare i dati alla classe evaluation
+
+
