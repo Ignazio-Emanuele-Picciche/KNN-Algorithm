89909b5f902833ddfa3dd79a0c0a3d909d9e0a0c
RiccardoPolacchi <riccardopolacchi@gmail.com>
2024-01-02 16:37:07 +0100

    creazione metodo caricamento dati

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Le funzioni della classe preprocessing sono:
-#caricare il dataset
-#gestire i valori mancanti
-#dividere i dati in features (x) e target label (y)
-#normalizzare o standardizzare i dati a seconda della scelta
-#passare i dati alla classe evaluation
+# Le funzioni della classe preprocessing sono:
+# caricare il dataset
+# gestire i valori mancanti
+# dividere i dati in features (x) e target label (y)
+# normalizzare o standardizzare i dati a seconda della scelta
+# passare i dati alla classe evaluation
 
+
+import pandas as pd
+
+
+class preprocessing():
+    def __init__(self):
+        pass
+
+    def caricamento_dataset(self, dati):    #creazione metodo caricamento dataset
+        self.dati = pd.read_csv('breast_cancer', index_col="Sample code number") #viene importato il dataset, e la prima colonna viene utilizzata per gli indici
 
