0faddc42fd81b52458615de634f089f4c8d4bf79
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2024-01-06 18:37:56 +0100

    Implementazione main, richiamato metodo scelta_metodo_evaluation

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from Preprocessing import Preprocessing
+from Input import Input
+from Evaluation import Evaluation
+from KNNAlgorithm import KNNAlgorithm
+
+if __name__ == '__main__':
+   input=Input()
+   preprocessing=Preprocessing()
+   metodo_di_valutazione,K=input.scelta_metodo_evaluation()
