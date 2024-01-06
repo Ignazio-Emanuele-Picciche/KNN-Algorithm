1287352c00858716c264a2d46df189bcf426f58d
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2024-01-06 19:05:15 +0100

    Implementazione main, richiamati metodi della classe Evaluation

--- main.py
+++ main.py
@@ -3,18 +3,20 @@
 from Preprocessing import Preprocessing
 from Input import Input
 from Evaluation import Evaluation
-from KNNAlgorithm import KNNAlgorithm
 
 if __name__ == '__main__':
-   input=Input()
-   preprocessing=Preprocessing()
-   metodo_di_valutazione,K=input.scelta_metodo_evaluation()
+    input = Input()
+    preprocessing = Preprocessing()
+    metodo_di_valutazione, K = input.scelta_metodo_evaluation()
 
-   preprocessing.caricamento_dataset()
-   preprocessing.pulizia_dati()
-   features,target=preprocessing.suddivisione_dati()
+    preprocessing.caricamento_dataset()
+    preprocessing.pulizia_dati()
+    features, target = preprocessing.suddivisione_dati()
+    perc_train = input.training
+    k = input.k
 
-
-
-
-
+    evaluation = Evaluation(features, target, perc_train, k)
+    if metodo_di_valutazione == 1:
+        evaluation.valutazione_holdout()
+    else:
+        evaluation.valutazione_random_subsampling(K)
