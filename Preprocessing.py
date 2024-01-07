e413f62d3475347d0e9a5e83a2fefca5777cb7f0
RiccardoPolacchi <riccardopolacchi@gmail.com>
2024-01-07 16:14:13 +0100

    eliminati dei return da alcuni metodi che erano inutili neel'utilizzo della classe nel main

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,7 +6,6 @@
 # gestire i valori mancanti
 # normalizzare o standardizzare i dati a seconda della scelta
 # dividere i dati in features (x) e target label (y)
-# passare i dati alla classe evaluation
 
 
 import pandas as pd
@@ -21,16 +20,16 @@ class Preprocessing(): #creo classe Preprocessing
     def caricamento_dataset(self):  # creazione metodo caricamento dataset
         self.dati = pd.read_csv('breast_cancer.csv', #importo il file csv
                                 index_col="Sample code number")  # viene importato il dataset, e la prima colonna viene utilizzata per gli indici
-        return self.dati
+
 
     def pulizia_dati(self): # creazione metodo pulizia dati che gestisce i valori mancanti
         self.dati = self.dati.dropna() # le righe corrispondenti ai valori mancanti vengono eliminate
-        return self.dati
+
 
     def standardizzazione(self): # creazione metodo standardizzazione per il feature scaling, da applicare a tutte le colonne tranne l'ultima (target label)
         for i in range(0, len(self.dati.columns) - 1):
             self.dati.iloc[:, i] = (self.dati.iloc[:, i] - self.dati.iloc[:, i].mean()) / self.dati.iloc[:, i].std()
-        return self.dati
+
 
     def suddivisione_dati(self):  # i dati vengono suddivisi in features (le x) e target label (le y). Entrambi dovranno avere il corrispondente indice
         x = self.dati.iloc[:, :-1]  # features (x)
