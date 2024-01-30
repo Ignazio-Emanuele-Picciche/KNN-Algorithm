d289da910f4e1e8acc6d55ac4b2040fd29d6fc57
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2024-01-30 11:15:41 +0100

    Modifica lista distanza

--- KNNAlgorithm.py
+++ KNNAlgorithm.py
@@ -44,7 +44,7 @@ class KNNAlgorithm:
                 # Calcolo della distanza euclidea tra il punto x_test ed i punti x_train
                 dist = self.calcolo_distanza_euclidea(punto_train, punto_test)  # metodo calcolo_distanza_euclidea
                 # Aggiungiamo alla lista distanze la coppia distanza,classe di appartenenza
-                distanze.append(((dist),self.y_train[index])) #loc mi restituisce la riga corrispondente all'indice
+                distanze.append(((dist),self.y_train[index]))
 
             # Ordiniamo in modo crescente le distanze
             distanze = sorted(distanze, key=itemgetter(0), reverse=True)
