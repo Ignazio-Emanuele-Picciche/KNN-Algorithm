db344dd0a6e583f9e9eea2fb8527154c99d24a01
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2024-01-30 11:56:52 +0100

    elimina commenti main

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from Input import Input
 from Evaluation import Evaluation
 
 if __name__ == '__main__ ':
-    input_utente = Input() # Istanziata la classe Input, acquisizione dati di input dall'utente
-    preprocessing = Preprocessing() # Istanziata la classe Preprocessing
-    metodo_di_valutazione, K = input_utente.scelta_metodo_evaluation() #Chiamata al metodo scelta_metodo_evaluation della classe Input, scelta del metodo di valutazione e del valore di K
+    input_utente = Input()
+    preprocessing = Preprocessing()
+    metodo_di_valutazione, K = input_utente.scelta_metodo_evaluation()
 
-    preprocessing.caricamento_dataset() #Chiamata al metodo caricamento_dataset della classe Preprocessing, caricamento del dataset
-    preprocessing.pulizia_dati() #Chiamata al metodo pulizia_dati della classe Preprocessing, pulizia del dataset
-    features, target = preprocessing.suddivisione_dati() #Chiamata al metodo suddivisione_dati della classe Preprocessing, suddivisione del dataset in features e target label
-    perc_train = input_utente.training #Percentuale di dati da utilizzare per l'addestramento del modello
-    k = input_utente.k # Acquisizione del valore di k dall'utente
+    preprocessing.caricamento_dataset()
+    preprocessing.pulizia_dati()
+    features, target = preprocessing.suddivisione_dati()
+    perc_train = input_utente.training
+    k = input_utente.k
 
-    evaluation = Evaluation(features, target, perc_train, k) # Istanziata la classe Evaluation, passaggio dei parametri features, target label, percentuale di dati da utilizzare per l'addestramento del modello e valore di k
-    if metodo_di_valutazione == 1: #Se il metodo di valutazione scelto dall'utente è holdout
-        evaluation.valutazione_holdout() #Chiamata al metodo valutazione_holdout della classe Evaluation
-    else:  #Se il metodo di valutazione scelto dall'utente è random subsampling
-        evaluation.valutazione_random_subsampling(K) #Chiamata al metodo valutazione_random_subsampling della classe Evaluation
+    evaluation = Evaluation(features, target, perc_train, k)
+    if metodo_di_valutazione == 1:
+        evaluation.valutazione_holdout()
+    else:
+        evaluation.valutazione_random_subsampling(K)
     # Creazione delle istanze delle classi Input e Preprocessing
     input_utente = Input()
     preprocessing = Preprocessing()
