f70b683c59d0bdaf2af589bf5ddfc057f1ef6cd2
AlessiaRossi <148058300+AlessiaRossi@users.noreply.github.com>
2024-02-06 17:46:38 +0100

    Tradotto input utente in inglese, corretto paramentro passato al costruttore dell'evaluation

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ from Evaluation import Evaluation
 
 if __name__ == '__main__':
     # Create instances of the Input and Preprocessing classes
-    input_utente = Input()
+    user_input = Input()
     preprocessing = Preprocessing()
 
     # Ask the user for the evaluation method and metrics to use
-    evaluation_method = input_utente.evaluation_method()
-    chosen_metrics = input_utente.metrics_selection()
-    K = input_utente.K
+    evaluation_method = user_input.evaluation_method()
+    chosen_metrics = user_input.metrics_selection()
+    K = user_input.K
 
     # Load, clean, and standardize the dataset
     preprocessing.loading_dataset()
     preprocessing.data_cleaning()
     preprocessing.standardization()
-    
+
     # Split the dataset into features and target label
     features, target = preprocessing.data_split()
 
     # Ask the user for the percentage of data to use for training and the number of neighbors to consider
-    training_perc = input_utente.training_percentage()
-    k = input_utente.k_neighbors()
+    training_perc = user_input.training_percentage()
+    k = user_input.k_neighbors()
 
     #  Create an instance of the Evaluation class to evaluate the model's performance
-    evaluation = Evaluation(features, target, training_perc, k, evaluation_method )
+    evaluation = Evaluation(features, target, training_perc, k, chosen_metrics)
 
-    if evaluation_method  == 1:
+    if evaluation_method == 1:
         evaluation.holdout_validation()
     else:
         evaluation.random_subsampling_validation(K)
